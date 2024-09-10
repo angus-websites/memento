c5f77444f52bdb17192ecc2b13dd44570d790302
Angus <angus.goody@gmail.com>
2024-09-10 16:12:06 +0100

    can view past words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -7,6 +7,19 @@ class WordsController < ApplicationController
     # Show all the past words of the day
     @words = WordOfDayRepository.new.all_words
   end
+
+  def show
+    # Find the word by its ID (params[:id] is automatically populated by Rails from the URL)
+    word_of_day = WordOfDay.find(params[:id])
+
+    # Assign the word's attributes to instance variables for the view
+    @word_of_the_day = word_of_day.word
+    @definition = word_of_day.definition
+    @phonetic = word_of_day.phonetic
+    @example = word_of_day.example
+    @date = word_of_day.date.strftime("%d %B %Y")
+  end
+
   def day
 
     # Show the current word of the day
