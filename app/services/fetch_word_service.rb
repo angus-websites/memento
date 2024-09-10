21b27503aa659d4e436989785248defb76d16d8b
Angus <angus.goody@gmail.com>
2024-09-10 11:13:12 +0100

    temp fetch word result

--- app/services/fetch_word_service.rb
+++ app/services/fetch_word_service.rb
@@ -4,13 +4,19 @@ class FetchWordService
   #
   # @return [WordDefinition]
   def fetch
-    response = YourApiClient.get_random_word
-
+    # response = YourApiClient.get_random_word
+    #
+    # WordDefinition.new(
+    #   word: response.word,
+    #   definition: response.definition,
+    #   phonetic: response.phonetic,
+    #   example: response.example
+    # )
     WordDefinition.new(
-      word: response.word,
-      definition: response.definition,
-      phonetic: response.phonetic,
-      example: response.example
-    )
+        word: "Memento",
+        definition: "Memento is a synonym of souvenir; it refers to something that is kept as a reminder of a person, place, or thing.",
+        phonetic: "məˈmɛntoʊ",
+        example: "I kept the shell as a memento of my visit to the seashore."
+      )
   end
 end
