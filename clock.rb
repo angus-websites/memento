2407896b167e7b4faa1b91b70ab9bc900e25f30c
Angus <angus.goody@gmail.com>
2024-09-18 16:21:03 +0100

    removed simple puts

--- clock.rb
+++ clock.rb
@@ -8,12 +8,6 @@ module Clockwork
     puts "Running #{job}"
   end
 
-  # Every minute of every day
-  every(1.minute, 'simple_puts:select') do
-    # Run the corresponding rake task
-    system('bundle exec rake simple_puts:select')
-  end
-
   # Select a word of the day at 12:00am every day
   every(1.day, 'word_of_day:select', at: '00:00') do
     system('bundle exec rake word_of_day:select')
--- lib/tasks/simple_puts.rake
+++
@@ -1,8 +0,0 @@
-# Simple puts task
-
-namespace :simple_puts do
-  desc "Simple puts task"
-  task select: :environment do
-    puts "Simple puts task"
-  end
-end
