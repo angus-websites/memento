791b8a825a8cb15d17825522158690fbc60c58aa
Angus <angus.goody@gmail.com>
2024-10-01 22:32:28 +0100

    basic word store

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,10 +5,25 @@ class SuggestionsController < ApplicationController
   def create
     captcha_validated, captcha_response = verify_hcaptcha
     if captcha_validated
-      puts("Captcha validated")
+      @suggestion = Suggestion.new(suggestion_params)
+      if @suggestion.save
+        flash[:notice] = "Suggestion submitted successfully."
+        redirect_to suggestions_new_path
+      else
+        flash[:error] = @suggestion.errors.full_messages.join(", ")
+        render :new
+      end
     else
       flash[:error] = captcha_response[:error_codes]
       render :new
     end
+  end
+
+  private
+
+  def suggestion_params
+    # Validates the parameters passed to the create action, ensures the data comes
+    # nested under the suggestion key, and permits only the specified attributes.
+    params.require(:suggestion).permit(:word, :definition, :phonetic, :example)
   end
 end
