d2df27d1a05cd0f8bdddfc58f4e433d058e51cd7
Angus <angus.goody@gmail.com>
2024-10-01 21:28:17 +0100

    installed hCaptcha

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem "pg"
 
 gem "clockwork"
 
+gem "hcaptcha", "~> 7.1"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,5 +3,11 @@ class SuggestionsController < ApplicationController
   end
 
   def create
+    if verify_hcaptcha
+      # Your existing code to handle form submission
+    else
+      flash[:alert] = "hCaptcha verification failed, please try again."
+      render :new
+    end
   end
 end
